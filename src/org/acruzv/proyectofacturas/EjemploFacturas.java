c8df846096d75f8af73105a231a91a6514df217f
alejandroCruzV <vcruz.alejandr@gmail.com>
2025-05-07 22:56:41 -0600

    optimizacion codigo Proyecto Facturas

--- src/org/acruzv/proyectofacturas/EjemploFacturas.java
+++ src/org/acruzv/proyectofacturas/EjemploFacturas.java
@@ -12,30 +12,34 @@ public class EjemploFacturas {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Ingrese descripcion para la factura");
-        String descFact = sc.nextLine();
+        //String descFact = sc.nextLine();
 
-        Factura factura = new Factura(cliente, descFact);
+        //Factura factura = new Factura(cliente, descFact);
+        Factura factura = new Factura(cliente, sc.nextLine());
 
         Producto producto;
-        String nombreProd;
+        /*String nombreProd;
         double precioProd;
-        int cantProd;
+        int cantProd;*/
 
         for (int i = 0; i < 3; i++) {
             producto = new Producto(); //Se creara un nuevo producto en cada iteracion
-            System.out.print("Ingrese nombre de producto N° " + producto.getCodigo()+": ");
-            nombreProd = sc.nextLine();
-            producto.setNombre(nombreProd);
+            System.out.print("Ingrese nombre de producto N° " + producto.getCodigo() + ": ");
+            //nombreProd = sc.nextLine();
+            //producto.setNombre(nombreProd);
+            producto.setNombre(sc.nextLine());
 
             System.out.print("Ingrese precio de producto: ");
-            precioProd = sc.nextDouble();
-            producto.setPrecio(precioProd);
+            //precioProd = sc.nextDouble();
+            //producto.setPrecio(precioProd);
+            producto.setPrecio(sc.nextDouble());
 
             System.out.print("Ingrese cantidad de producto: ");
-            cantProd = sc.nextInt();
+            //cantProd = sc.nextInt();
 
-            ItemFactura item = new ItemFactura(producto, cantProd);
-            factura.addItemFactura(item);//se agrega el item al array de ItemFactura
+            //ItemFactura item = new ItemFactura(producto, cantProd);
+            //factura.addItemFactura(item);//se agrega el item al array de ItemFactura
+            factura.addItemFactura(new ItemFactura(producto, sc.nextInt()));
 
             System.out.println();
             sc.nextLine();//Para mover el cursor de scanner a la siguiente linea al finalizar cada iteracion
